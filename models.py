c6a93211e838ed4b3466b7a2f1a77caccb4b1996
Will Hewitt <me@hewittwill.com>
2020-03-26 15:17:21 +1300

    Changed to add a 3rd conv layer

--- models.py
+++ models.py
@@ -14,6 +14,7 @@ def simple_covid_net():
 
     covid_net.add(k.layers.Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(224,224,1)))
     covid_net.add(k.layers.Conv2D(64, kernel_size=(3,3), activation='relu'))
+    covid_net.add(k.layers.Conv2D(128, kernel_size=(3,3), activation='relu'))
     covid_net.add(k.layers.MaxPooling2D(pool_size=(2,2)))
     covid_net.add(k.layers.Dropout(0.25))
     covid_net.add(k.layers.Flatten())
